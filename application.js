2e7dbfee6788a37ece473d2b214f9776c2ecdfca
Zuzana Dostalova <zuzana.dostalova@icloud.com>
2014-06-23 23:41:07 +0200

    names and commits count

--- application.js
+++ application.js
@@ -1,12 +1,51 @@
-$(document).ready(function(){
-  // styling all the headings with pink color
-  d3.selectAll("h1").style("color", "#ef7591");
-  
-  var dataset = [1, 33, 42, 24, 8]
-  d3.select(".col-md-8").selectAll("p")
-                   .data(dataset)
-                   .enter()
-                   .append("p")
-                   .text(function(d) { return d; });
-  
+$(document).ready(function() {
+
+  $("input").change(function() {
+    var username = $(this).val();
+    var all_repos = $.get("https://api.github.com/users/" + username + "/repos")
+      .done(function(data) {
+        window.names = getNames(data);
+        getCommits(window.names, username, function(commits) {
+          console.log(commits);
+        });
+
+
+        console.log(names)
+
+      });
+  });
 });
+
+function getNames(data) {
+  var names = [];
+  for (var i = 0; i < data.length; i++) {
+    names.push(data[i]["name"])
+  }
+
+  return names;
+}
+
+function getCommits(names, user, callback) {
+  var count = names.length,
+      curr = 0,
+      acc = []
+
+  for (var i = 0; i < names.length; i++) {
+    $.get("https://api.github.com/repos/" + user + "/" + names[i] + "/commits")
+      .done(function(data) {
+        acc.push(data.length),
+        curr++;
+        if (curr === count) {
+          callback(acc);
+        }
+      });
+  }
+}
+
+function drawChart(data) {
+  d3.select(".repos-chart").selectAll("div")
+    .data(data)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+}
